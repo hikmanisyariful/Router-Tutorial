688de33acc4eac58a2e382d2e8eb611f71f5c473
hikmanisyariful <syarifulfajar017@gmail.com>
2022-02-24 12:59:03 +0700

    Reading URL Params - Step 1 > Setup Route & Outlet
    A couple things to note:
    
    1. We just created a route that matches urls like "/invoices/2005" and
    "/invoices/1998". The :invoiceId part of the path is a "URL param",
    meaning it can match any value as long as the pattern is the same.
    
    2. The <Route> adds a second layer of route nesting when it matches:
    <App><Invoices><Invoice /></Invoices></App>. Because the <Route>
    is nested the UI will be nested too.

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Expenses from "./routes/expenses";
 import Invoices from "./routes/invoices";
+import Invoice from "./routes/invoice";
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} />
+        <Route path="invoices" element={<Invoices />}>
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
         <Route
           path="*"
           element={
---
+++ src/routes/invoice.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function invoice() {
+  return <h2>Invoice #???</h2>;
+}
