c752110b16fc61407ac91e848a23051851f05683
hikmanisyariful <syarifulfajar017@gmail.com>
2022-02-24 09:36:49 +0700

    Add Some routes
    Notice at "/" it renders <App>. At "/invoices" it render <Invoices>.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+    </Routes>
   </BrowserRouter>,
   document.getElementById("root")
 );
